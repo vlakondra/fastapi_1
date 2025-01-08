04e6dfe45aa2a12cd823ad8d2b21d39851fff1b4
vlakondra <kondrashkin.wladimir@yandex.ru>
2025-01-09 00:49:04 +0500

    `Update main.py and data/studs.json files, add json import to main.py, modify FastAPI routes and models.`

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 '''
 main module!
 '''
+import json
+
 from fastapi import FastAPI
-# from fastapi.responses import JSONResponse, FileResponse
-# from mod import ModelName
-# import mod
+from fastapi.responses import  FileResponse
+
+
 
 #https://fastapi.xiniushu.com/ru/
 
-# from peewee import SqliteDatabase
-# from playhouse.reflection import generate_models, print_model, print_table_sql
+from peewee import SqliteDatabase
+from playhouse.reflection import generate_models#, print_model, print_table_sql
 
-# db = SqliteDatabase('chinook.db', pragmas={'foreign_keys': 1})
+db = SqliteDatabase('chinook.db', pragmas={'foreign_keys': 1})
+models = generate_models(db)
 
-# models = generate_models(db)
-# art = models['artists']
 
 app = FastAPI(title ="This Is FastAPI")
-
-app = FastAPI()
 
 class Book:
     def __init__(self, title: str, author: str) -> None:
@@ -37,24 +36,44 @@ class Book:
 @app.get("/")
 async def root():
     '''  root  '''
-    # b = Book('zz',['xx'])
-    # print(b)
-    return {'data':123} ##FileResponse('index.html')
 
+    return  {'data':123}
 
+@app.get("/students/")
+async def stud_data():
+    '''students'''
+    with open('data/studs.json', 'r', encoding='utf-8') as file:
+        json_str = file.read()
+        dict_list = json.loads(json_str)
 
+    return dict_list
+
+@app.get("/findart/")
+async def find_art():
+    return FileResponse('index.html')
+
+@app.get("/artists/{firsts}")
+async def art_data(firsts:str):
+    '''Исполнители'''
+    art = models['artists']
+    query = (art
+           .select()
+           .where(art.name.startswith(firsts))
+           .dicts()
+    )
+    return list(query)
 
 @app.get("/first/{quant}")
 async def func_first(quant: int):
     '''тестовая функция'''
 
     if not isinstance(quant, int) or quant < 0:
-        return {'error': 'Input must be a non-negative integer.'}
+        return {'error': 'Параметр должен быть положительным целым числом.'}
     try:
-        b = Book('zz',[99]) #Аннотация типов не препятствует выполнению при неверном типе значения
-        # print(b.author)
+        b = Book('Толстой',[99]) #Аннотация типов не препятствует выполнению при неверном типе значения
+
         if quant > 10:
-            raise ValueError('Input is too big, must be 10 or less.')
+            raise ValueError('Значение парметра не должно превышать 10')
         return {'param': quant ** 2,"data": b.author}
     except ValueError as e:
         return {'Error': str(e)}
