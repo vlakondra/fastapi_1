29c3ba52a2f75924fa07d4d84e83760b9cc8c9a6
vlakondra <kondrashkin.wladimir@yandex.ru>
2025-02-02 23:59:29 +0500

    `Refactor main.py and update routes to use path and query parameters`

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ main module!
 import json
 from typing import List, Dict
 
+
 #Модули FastApi
 from fastapi import FastAPI
 from fastapi.responses import  FileResponse
@@ -19,6 +20,17 @@ models = generate_models(db)
 
 app = FastAPI(title ="This Is FastAPI")
 
+
+def memoize(func):
+    cache = {}
+    def wrapper(*args):
+        if args in cache:
+            return cache[args]
+        result = func(*args)
+        cache[args] = result
+        return result
+    return wrapper
+
 class Book:
     def __init__(self, title: str, author: str) -> None:
         self.title = title
@@ -26,16 +38,17 @@ class Book:
 
 #События инициализации FastApi
 @app.on_event("startup")
-async def startup():
+async def startup() -> None:
     print("Стартует...")
 
 @app.on_event("shutdown")
 async def shutdown():
     print("Закрывается...")
 
-@app.get("/")
-async def root():
+@app.get("/{myid}")
+async def root(myid:int):
     '''  Основной маршрут  '''
+    print('myid',myid)
     return  {'data':123}
 
 @app.get("/students/")
@@ -53,16 +66,16 @@ async def find_art():
     '''Возвращает интерактивный HTML-файл для поиска исполнителей'''
     return FileResponse('index.html')
 
-@app.get("/artists/{firsts}")
-async def art_data(firsts:str) -> List[Dict]:
+@app.get("/artists/{lett}")
+async def art_data(lett:str) -> List[Dict]:
     '''Возвращает найденных Исполнителей'''
     art = models['artists']
     query = (art
            .select()
-           .where(art.name.startswith(firsts))
+           .where(art.name.startswith(lett))
            .dicts()
     )
-    return list(query)
+    return query
 
 @app.get("/first/{quant}")
 async def func_first(quant: int):
@@ -71,7 +84,7 @@ async def func_first(quant: int):
     if not isinstance(quant, int) or quant < 0:
         return {'error': 'Параметр должен быть положительным целым числом.'}
     try:
-        b = Book('Толстой',[99]) #Аннотация типов не препятствует выполнению при неверном типе значения
+        b = Book('Толстой', 'Author Name')  # Correcting the author parameter to be a string
 
         if quant > 10:
             raise ValueError('Значение параметра не должно превышать 10')
